5d070373e633837bae59d1b107edabf70015fe76
DominikHor <dhorstkamp@aol.com>
2016-07-07 10:45:28 +0200

    setup

---
+++ conf.lua
@@ -0,0 +1,8 @@
+function love.conf(t)
+  t.title = "Catch me if u can"
+  t.version = "0.10.1"
+  t.window.width  = 1024
+  t.window.heigth = 720
+
+  t.console = true
+end
--- main.lua
+++ main.lua
@@ -1,3 +1,17 @@
+gameTime = 0
+
+function love.load()
+	player = {
+		x = 512,
+		y = 512
+	}
+end
+
 function love.draw()
-    love.graphics.print("Hello World", 400, 300)
+	love.graphics.rectangle("fill", player.x, player.y, 32, 32)
+end
+
+function love.update(dt)
+  gameTime = gameTime + dt
+  player.x = 512 + math.sin(gameTime)*100
 end
