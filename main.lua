863145740eec9c7e8a06766cf212961192534a30
ThiloSchnelle <thilo.schnelle@web.de>
2016-07-07 11:19:05 +0200

    Simple
    Simple

--- main.lua
+++ main.lua
@@ -1,17 +1,34 @@
 gameTime = 0
+players = { }
 
 function love.load()
-	player = {
+	player = { 
 		x = 512,
-		y = 512
-	}
+		y = 512,
+		image = love.graphics.newImage('assets/first.png'),
+		name = "first"
+	} 
+	table.insert(players, player)
+	
+	player2 = { 
+		x = 256,
+		y = 256,
+		image = love.graphics.newImage('assets/second.png'),
+		name = "second"
+	} 
+	
+	table.insert(players, player)
 end
 
 function love.draw()
-	love.graphics.rectangle("fill", player.x, player.y, 32, 32)
+	for i, player in ipairs(players) do
+        love.graphics.draw(player.image, player.x, player.y)
+    end
 end
 
 function love.update(dt)
   gameTime = gameTime + dt
-  player.x = 512 + math.sin(gameTime)*100
+  for i, player in ipairs(players) do
+        player.x = math.sin(gameTime)*100
+  end
 end
